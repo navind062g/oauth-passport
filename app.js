0c8cea2ca8a8d90ff337755c9fce908d4a85b6f1
navind062g <62036788+navind062g@users.noreply.github.com>
2022-12-23 21:58:49 -0600

    Handling Callback and Authentication with Google

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const authRoutes = require('./routes/auth-routes');
 const passportSetup = require('./config/passport-setup.js');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config(); 
 
 const app = express();
 
-//set up new engine
+//set up view engine
 app.set('view engine', 'ejs');
+
+//set up the mongo database
+mongoose.connect(process.env.MONGO_DB_STRING, () => {
+    console.log('Connected to Mongo DB.');
+})
 
 //set up routes
 app.use('/auth', authRoutes);
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const dotenv = require('dotenv').config();
+const User = require('../models/user-model');
 
 passport.use(
     new GoogleStrategy({
@@ -13,5 +14,20 @@ passport.use(
         //passport callback function
         console.log('You have reached the callback function');
         console.log(profile);
+
+        User.findOne({googleId: profile.id}).then((currentUser) => {
+            if(currentUser) {
+                console.log("Existing User", currentUser);
+            }
+            else {
+                new User({
+                    userName: profile.displayName,
+                    googleId: profile.id
+                }).save().then((newUser) => {
+                    console.log('New User has been created: '+newUser);
+                }
+                );
+            }
+        });
     })
 );
